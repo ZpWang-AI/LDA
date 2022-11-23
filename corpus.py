8753d58440628962e97adb1a91fa57af546f7980
wzp <zhipangwang@gmail.com>
2022-11-23 12:59:32 +0800

    add corpus

---
+++ corpus.py
@@ -0,0 +1,52 @@
+import re
+import os
+import jieba as jb
+import jieba.posseg as jbp
+
+from tqdm import tqdm 
+from pathlib import Path as path
+
+
+class Corpus:
+    def __init__(self, root_fold, 
+                 stopwords=('没', '就', '知道', '是', '才', '听听', '坦言', '全面', '越来越', '评价', '放弃', '人'),
+                 tags=('n', 'nr', 'ns', 'nt', 'eng', 'v', 'd'),
+                 ) -> None:
+        self.root_fold = path(root_fold)
+        self.stopwords = stopwords
+        self.tags = tags
+        self.dic = None
+        
+    def __iter__(self):
+        return self.get_iter()
+    
+    def split_line(self, line):
+        return re.split(r'[.!?。！？…]', line)
+    
+    def cut_sentence(self, sentence):
+        return [w for w in jb.cut(sentence) if w not in self.stopwords]
+        # return [w.word for w in jbp.cut(sentence) if w.flag in self.tags and w.word not in self.stopwords]
+    
+    def read_txt(self, file_path):
+        with open(file_path, 'r', encoding='utf-8')as f:
+            for line in f.readlines():
+                for sentence in self.split_line(line):
+                    yield sentence
+    
+    def get_iter(self):
+        for son_file in tqdm(os.listdir(self.root_fold)):
+            cur_file = self.root_fold/son_file
+            for sentence in self.read_txt(cur_file):
+                sentence = sentence.strip()
+                if sentence:
+                    sentence = self.cut_sentence(sentence)
+                    if self.dic is None:
+                        yield sentence
+                    else:
+                        yield self.dic.doc2bow(sentence)
+                
+
+if __name__ == '__main__':
+    for a in Corpus('./data_txt/'):
+        print(a)
+    
---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+import jieba.posseg as jb
+
+from gensim import corpora, models
+from pathlib import Path as path
+
+from corpus import Corpus
+
+
+# TODO: set data_fold and stopwords
+cur_corpus = Corpus(path('data_txt'), stopwords=['你', '我', '他'])
+
+print('start generating dictionary')
+dictionary = corpora.Dictionary(cur_corpus)
+print('finish generating dictionary')
+cur_corpus.dic = dictionary
+
+print('start training model')
+# TODO: set num_topics
+lda_model = models.LdaModel(corpus=cur_corpus, id2word=dictionary, num_topics=10)
+print('finish training model')
+
+for topic in lda_model.print_topics(num_words=100):
+    print(topic)
